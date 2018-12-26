410757d3aa4bc4c239d0fae0772f4e79b39cca40
lesch <leschinskysergey@gmail.com>
2018-12-26 23:15:51 +0200

    Added RemoveDuplicates

--- CTCI/UniqueString.java
+++ CTCI/UniqueString.java
@@ -1,7 +1,9 @@
 package CTCI;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class UniqueString {
 
@@ -22,18 +24,45 @@ public class UniqueString {
         return (map.values().stream().filter((x)-> x > 1).count() == 0);
     }
 
-    public static void main(String[] args){
-        String test = "A"; //true
-        String test2 = "ABA"; // false
-        String test3 = "ABCDAEF"; // false
-        String test4 = "AaBbCcDd"; // true
-        String test5 = "!ANORMaLWorD!"; //false
+    public static String removeDuplicates(String s){
+        int index = 0;
+        StringBuilder stringBuilder = new StringBuilder(s);
+        while(index != stringBuilder.length()){
+            Character c = stringBuilder.charAt(index);
+            if(stringBuilder.substring(0,index).contains(c.toString())){
+                stringBuilder.deleteCharAt(index);
+                continue;
+            } else{
+                index++;
+            }
+        }
+        return stringBuilder.toString();
+    }
 
-        System.out.println( uniqueChecker(test));
-        System.out.println( uniqueChecker(test2));
-        System.out.println( uniqueChecker(test3));
-        System.out.println( uniqueChecker(test4));
-        System.out.println( uniqueChecker(test5));
+    public static void main(String[] args){
+        String test1 = "A"; //true
+        String test1v2 = "ABA"; // false
+        String test1v3 = "ABCDAEF"; // false
+        String test1v4 = "AaBbCcDd"; // true
+        String test1v5 = "!ANORMaLWorD!"; //false
+
+        String test2 = "AAB"; //AB
+        String test2v1 = "ABBAAAAA"; //AB
+        String test2v2 = "ACBCBCCBCGDF"; //ACBGDF
+        String test2v3 = "ABCDFDCBA"; // ABCDF
+        String test2v4 = "AABBCCDDEEFF"; //ABCDEF
+
+        System.out.println( uniqueChecker(test1));
+        System.out.println( uniqueChecker(test1v2));
+        System.out.println( uniqueChecker(test1v3));
+        System.out.println( uniqueChecker(test1v4));
+        System.out.println( uniqueChecker(test1v5));
+
+        System.out.println( removeDuplicates(test2));
+        System.out.println( removeDuplicates(test2v1));
+        System.out.println( removeDuplicates(test2v2));
+        System.out.println( removeDuplicates(test2v3));
+        System.out.println( removeDuplicates(test2v4));
 
     }
 }
